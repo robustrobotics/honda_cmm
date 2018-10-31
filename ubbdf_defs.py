c270daa3b4abb415bb2708ae2c856900524ecc7a
Michael Noseworthy <mnoseworthy7@gmail.com>
2018-10-30 20:57:47 -0400

    Add the update_light relation

--- ubbdf_defs.py
+++ ubbdf_defs.py
@@ -1,13 +1,18 @@
 # classes used for storing information from the parsed UBBDF
+import pybullet as p
+
+
 class Link(object):
     def __init__(self, name):
         self.name = name
+
 
 class Joint(object):
     def __init__(self, name, type, child_link):
         self.name = name
         self.type = type
         self.child_link = child_link
+
 
 class Relation(object):
     def __init__(self, name, parent_joint, child_joint, type, params):
@@ -28,12 +33,35 @@ class Relation(object):
 # relation functions for checking if relation has been activated and updating the
 # world if it has
 
+
 # discrete examples
 def update_light(world, parent_joint, child_joint, params):
-    return True
+    """
+    :param world: A dictionary describing the busybox model (links, joints, relations).
+    :param parent_joint: The name of the joint describing the activation constraint.
+    :param child_joint: The name of the joint whose color will change upon activation.
+    :param params: The following params should be specified in the xml:
+                    threshold: the value of the parent_joint to toggle the light
+                    off_color: color of the deactivated light
+                    on_color: color of the activated light
+    """
+    on_color = [float(s) for s in params['on_color'].split()]
+    off_color = [float(s) for s in params['off_color'].split()]
+    joint_state = p.getJointState(world['model_id'], parent_joint.pybullet_id)[0]
+
+    if joint_state > float(params['threshold']):
+        p.changeVisualShape(objectUniqueId=world['model_id'],
+                            linkIndex=world['links'][child_joint.child_link].pybullet_id,
+                            rgbaColor=on_color)
+    else:
+        p.changeVisualShape(objectUniqueId=world['model_id'],
+                            linkIndex=world['links'][child_joint.child_link].pybullet_id,
+                            rgbaColor=off_color)
+
 
 def update_sound(world, parent_joint, child_joint, params):
     return True
+
 
 # continuous example
 def update_door(world, parent_joint, child_joint, params):
