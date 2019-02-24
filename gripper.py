a0b2d770236aa9d2d13a200f1d08290bc456b6f7
Caris Moses <carism@mit.edu>
2019-02-24 14:48:25 -0500

    created method for applying a force at the tip of the gripper and added open and close gripper function to it

--- gripper.py
+++ gripper.py
@@ -5,12 +5,12 @@ from util import transformation
 class Gripper:
     def __init__(self):
         self.id = p.loadSDF("models/gripper/gripper.sdf")[0]
-        p.resetBasePositionAndOrientation(self.id,[-0.100000,0.000000,0.070000],[0.000000,0.000000,0.000000,1.000000])
         self.left_finger_tip_id = 2
         self.right_finger_tip_id = 5
         self.left_finger_base_joint_id = 0
         self.right_finger_base_joint_id = 3
         self.pose_tip_base = self.get_pose_tip_base()
+        self.finger_force = 15
 
     def get_pose_tip_base(self):
         p_base_w, orn_base_w = p.getBasePositionAndOrientation(self.id)
@@ -26,17 +26,22 @@ class Gripper:
         p.resetBasePositionAndOrientation(self.id, p_base_world_des, pose_tip_w_des[1])
         p.stepSimulation()
 
-    def set_gripper_state(self, des_state):
-        if des_state == 'open':
+    def apply_force(self, force=None, finger_state='close'):
+        if force is not None:
+            # apply a force at the center of the gripper finger tips
+            p_tip_base = self.pose_tip_base[0]
+            p_base_w, orn_base_w = p.getBasePositionAndOrientation(self.id)
+            p_tip_w = transformation(p_tip_base, p_base_w, orn_base_w)
+            p.applyExternalForce(self.id, -1, force, p_tip_w, p.WORLD_FRAME)
+
+        if finger_state == 'open':
             finger_angle = 0.2
-        elif des_state == 'close':
+        elif finger_state == 'close':
             finger_angle = 0.0
-        else:
-            print('desired gripper state can only be open or close')
 
-        p.setJointMotorControl2(self.id,self.left_finger_base_joint_id,p.POSITION_CONTROL,targetPosition=-finger_angle)#,force=self.fingerAForce)
-        p.setJointMotorControl2(self.id,self.right_finger_base_joint_id,p.POSITION_CONTROL,targetPosition=finger_angle)#,force=self.fingerBForce)
+        p.setJointMotorControl2(self.id,self.left_finger_base_joint_id,p.POSITION_CONTROL,targetPosition=-finger_angle,force=self.finger_force)
+        p.setJointMotorControl2(self.id,self.right_finger_base_joint_id,p.POSITION_CONTROL,targetPosition=finger_angle,force=self.finger_force)
+        p.setJointMotorControl2(self.id,2,p.POSITION_CONTROL,targetPosition=0,force=self.finger_force)
+        p.setJointMotorControl2(self.id,5,p.POSITION_CONTROL,targetPosition=0,force=self.finger_force)
 
-        #p.setJointMotorControl2(self.kukaUid,10,p.POSITION_CONTROL,targetPosition=0,force=self.fingerTipForce)
-        #p.setJointMotorControl2(self.kukaUid,13,p.POSITION_CONTROL,targetPosition=0,force=self.fingerTipForce)
         p.stepSimulation()
