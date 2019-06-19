d95aedc54e026af78e3ff9e769bc84c7ba3d95f6
Caris Moses <carism@mit.edu>
2019-06-18 22:16:50 -0400

    adding function to test reading in pickle file

--- gen/generate_policy_data.py
+++ gen/generate_policy_data.py
@@ -15,18 +15,28 @@ def generate_data(file_name, n_samples, viz, debug):
             pickle.dump(samples, handle)
     print('done generating dataset')
 
+def test_read(file_name):
+    fname = file_name + '.pickle'
+    with open(fname, 'rb') as handle:
+        results = pickle.load(handle)
+        print('successfully read in pickle file')
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--viz', action='store_true')
     parser.add_argument('--debug', action='store_true')
     parser.add_argument('--n-samples', type=int, default=5)
     parser.add_argument('--fname', type=str) # give filename (without .pickle)
+    parser.add_argument('--test-read', action='store_true')
     args = parser.parse_args()
 
     if args.debug:
         import pdb; pdb.set_trace()
 
-    generate_data(args.fname, args.n_samples, args.viz, args.debug)
+    if not args.test_read:
+        generate_data(args.fname, args.n_samples, args.viz, args.debug)
+    else:
+        test_read(args.fname)
 
     try:
         while True:
