d0bc0fde51267996992abd69b20af9a16267a4b1
Caris Moses <carism@mit.edu>
2019-06-19 14:01:15 -0400

    change append to expend so pickled list is of Results not lists. also no way to overwrite file when just want to --test-read

--- gen/generate_policy_data.py
+++ gen/generate_policy_data.py
@@ -1,14 +1,13 @@
-import pickle
 import sys
 import argparse
 from test_policy import test_policy
 from util import util
 
-samples = []
-def generate_data(n_samples, viz, debug, git_hash):
+results = []
+def generate_data(n_results, viz, debug, git_hash):
     for i in range(n_samples):
         sys.stdout.write("\rProcessing sample %i/%i" % (i+1, n_samples))
-        samples.append(test_policy(viz=viz, debug=debug, max_mech=1, random=True, git_hash=git_hash))
+        results.extend(test_policy(viz=viz, debug=debug, max_mech=1, random=True, git_hash=git_hash))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -20,31 +19,32 @@ if __name__ == '__main__':
     parser.add_argument('--save-git', action='store_true')
     args = parser.parse_args()
 
-    try:
-        if args.debug:
-            import pdb; pdb.set_trace()
+    if args.debug:
+        import pdb; pdb.set_trace()
 
-        if not args.test_read:
+
+    if args.test_read:
+        data = util.read_from_file(args.fname)
+    else:
+        try:
             git_hash = None
             if args.save_git:
                 import git
                 repo = git.Repo(search_parent_directories=True)
                 git_hash = repo.head.object.hexsha
             generate_data(args.n_samples, args.viz, args.debug, git_hash)
-            util.write_to_file(args.fname, samples)
-        else:
-            data = util.read_from_file(args.fname)
-    except KeyboardInterrupt:
-        # if Ctrl+C write to pickle
-        util.write_to_file(args.fname, samples)
-        print('Exiting...')
-    except:
-        # if crashes write to pickle
-        util.write_to_file(args.fname, samples)
+            util.write_to_file(args.fname, results)
+        except KeyboardInterrupt:
+            # if Ctrl+C write to pickle
+            util.write_to_file(args.fname, results)
+            print('Exiting...')
+        except:
+            # if crashes write to pickle
+            util.write_to_file(args.fname, results)
 
-        # for post-mortem debugging since can't run module from command line in pdb.pm() mode
-        import traceback, pdb, sys
-        traceback.print_exc()
-        print('')
-        pdb.post_mortem()
-        sys.exit(1)
+            # for post-mortem debugging since can't run module from command line in pdb.pm() mode
+            import traceback, pdb, sys
+            traceback.print_exc()
+            print('')
+            pdb.post_mortem()
+            sys.exit(1)
