8c520d9495c4b25c46cc02acb49b7039de96e810
Caris Moses <carism@mit.edu>
2019-06-19 09:53:11 -0400

    now write to pickle file when ctrl+c or crash. much faster data collection

--- gen/generate_policy_data.py
+++ gen/generate_policy_data.py
@@ -3,23 +3,24 @@ import sys
 import argparse
 from test_policy import test_policy
 
-def generate_data(file_name, n_samples, viz, debug):
-    samples = []
+samples = []
+def generate_data(n_samples, viz, debug):
     for i in range(n_samples):
         sys.stdout.write("\rProcessing sample %i/%i" % (i+1, n_samples))
-        samples += test_policy(viz=viz, debug=debug, max_mech=1, random=True)
+        samples.append(test_policy(viz=viz, debug=debug, max_mech=1, random=True))
 
-        # save to pickle (keep overwriting latest file in case it crashes)
-        fname = file_name + '.pickle'
-        with open(fname, 'wb') as handle:
-            pickle.dump(samples, handle)
-    print('done generating dataset')
+def write_to_file(file_name):
+    # save to pickle
+    fname = file_name + '.pickle'
+    with open(fname, 'wb') as handle:
+        pickle.dump(samples, handle)
+    print('\nwrote dataset to '+fname)
 
 def test_read(file_name):
     fname = file_name + '.pickle'
     with open(fname, 'rb') as handle:
         results = pickle.load(handle)
-        print('successfully read in pickle file')
+        print('successfully read in '+fname)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -28,20 +29,24 @@ if __name__ == '__main__':
     parser.add_argument('--n-samples', type=int, default=5)
     parser.add_argument('--fname', type=str) # give filename (without .pickle)
     parser.add_argument('--test-read', action='store_true')
-    parser.add_argument('--pdb', action='store_true') # if want pdb but not all print outs
     args = parser.parse_args()
 
     try:
-        if args.debug or args.pdb:
+        if args.debug:
             import pdb; pdb.set_trace()
 
         if not args.test_read:
-            generate_data(args.fname, args.n_samples, args.viz, args.debug)
+            generate_data(args.n_samples, args.viz, args.debug)
         else:
             test_read(args.fname)
     except KeyboardInterrupt:
+        # if Ctrl+C write to pickle
+        write_to_file(args.fname)
         print('Exiting...')
     except:
+        # if crashes write to pickle
+        write_to_file(args.fname)
+
         # for post-mortem debugging since can't run module from command line in pdb.pm() mode
         import traceback, pdb, sys
         traceback.print_exc()
