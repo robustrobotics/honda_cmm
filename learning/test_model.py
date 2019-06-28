d40deb5699841e5f34ea614eaa3d5b2ce8f1acdf
Caris Moses <carism@mit.edu>
2019-06-28 12:24:32 -0400

    can load pol or polvis model type from file

--- learning/test_model.py
+++ learning/test_model.py
@@ -2,24 +2,34 @@
 import argparse
 import operator
 from actions import policies
-from setup_pybullet import random_env
+from setup_pybullet import setup_env
 from scipy.optimize import minimize
+from learning.nn_disp_pol import DistanceRegressor as NNPol
 from learning.nn_disp_pol_vis import DistanceRegressor as NNPolVis
+from learning.dataloaders import parse_pickle_file
 import torch
+import torchvision.transforms as transforms
+from util import util
 
 def objective_func(x, k, image, model):
     return -model.forward(k, x[0], x[1], image)
 
-def test_random_env(model_path, viz, max_mech, debug):
+def test_random_env(model_path, mode, viz, debug):
     # TODO: ability to later visualize when testing max policy, but not during sampling phase
-    bb, gripper, image_data = random_env(viz=False, max_mech=1, debug=debug)
-    model = NNPolVis(policy_names=['Prismatic', 'Revolute'],
-                   policy_dims=[10, 14],
-                   hdim=args.hdim,
-                   im_h=154,
-                   im_w=205,
-                   kernel_size=5).cuda()
-    model.load_state_dict(torch.load('data/model.pt'))
+    bb, gripper, image_data = setup_env(viz=False, max_mech=1, debug=debug)
+    if args.model == 'pol':
+        model = NNPol(policy_names=['Prismatic', 'Revolute'],
+                    policy_dims=[10, 14],
+                    hdim=16)
+        model.load_state_dict(torch.load('data/pol_model.pt'))
+    else:
+        model = NNPolVis(policy_names=['Prismatic', 'Revolute'],
+                       policy_dims=[10, 14],
+                       hdim=16,
+                       im_h=154,
+                       im_w=205,
+                       kernel_size=5)
+        model.load_state_dict(torch.load('data/polvis_model.pt'))
     model.eval()
 
     # only testing with 1 Mechanism per BusyBox right now
@@ -29,13 +39,16 @@ def test_random_env(model_path, viz, max_mech, debug):
 
     for _ in range(n_samples):
         random_policy = policies.generate_random_policy(bb, mech)
-        k = random_policy.type
-        theta = random_policy.get_params_tuple()
         q = random_policy.generate_random_config()
-        image= image_data.rgbPixels
-        samples += [[(k, theta, q, im), model.forward(k, theta, q, im)]]
+        policy_tuple = random_policy.get_policy_tuple()
+        #mech_tuple = mech.get_mechanism_tuple()
+        data = [util.Result(None, policy_tuple, None, None, 0., None, None, q, image_data, None)]
+        tensor_data = parse_pickle_file(data=data)[0]
+        policy_type = random_policy.type
+        samples += [[(tensor_data['params'], tensor_data['config']), \
+                        model.forward(policy_type, tensor_data['params'], tensor_data['config'], tensor_data['image'])]]
 
-    (k_max, theta_max, q_max, im), max_output = max(samples, key=operator.itemgetter(1))
+    (theta_max, q_max), max_output = max(samples, key=operator.itemgetter(1))
 
     # start optimization from here
     # assume you guessed the correct policy type, and optimize for params and configuration
@@ -60,11 +73,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--viz', action='store_true')
     parser.add_argument('--debug', action='store_true')
-    parser.add_argument('--max-mech', type=int, default=1)
     parser.add_argument('--model-path', type=str)
     args = parser.parse_args()
 
     if args.debug:
         import pdb; pdb.set_trace()
 
-    test_random_env(args.model_path, args.viz, args.max_mech, args.debug)
+    test_random_env(args.model_path, args.viz, args.debug)
