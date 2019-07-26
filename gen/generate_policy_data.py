25b798fe22a35682b5d3c1c9670ff194582f17c8
Caris Moses <carism@mit.edu>
2019-07-26 13:54:36 -0400

    can sample number of bbs and number of interactions with bbs

--- gen/generate_policy_data.py
+++ gen/generate_policy_data.py
@@ -9,46 +9,47 @@ from actions import policies
 from actions.gripper import Gripper
 from gen.generator_busybox import Slider, Door, BusyBox
 
-def generate_samples(viz, debug, max_mech, match_policies, randomness,
+def generate_samples(n_samples, viz, debug, max_mech, match_policies, randomness,
                             goal_config, bb=None, git_hash=None, tag=''):
     if bb is None:
         bb = BusyBox.generate_random_busybox(max_mech=max_mech, mech_types=[Slider], urdf_tag=tag, debug=debug)
 
-    # setup env and get image before load gripper
-    image_data = setup_env(bb, viz, debug)
-    gripper = Gripper(bb.bb_id)
-
     results = []
-    for mech in bb._mechanisms:
-        # generate either a random or model-based policy and goal configuration
-        policy = policies.generate_policy(bb, mech, match_policies, randomness)
-        config_goal = policy.generate_config(mech, goal_config)
-        pose_handle_world_init = util.Pose(*p.getLinkState(bb.bb_id, mech.handle_id)[:2])
+    for _ in range(n_samples):
+        # setup env and get image before load gripper
+        image_data = setup_env(bb, viz, debug)
+        gripper = Gripper(bb.bb_id)
 
-        # calculate trajectory
-        pose_handle_base_world = mech.get_pose_handle_base_world()
-        traj = policy.generate_trajectory(pose_handle_base_world, config_goal, debug)
+        for mech in bb._mechanisms:
+            # generate either a random or model-based policy and goal configuration
+            policy = policies.generate_policy(bb, mech, match_policies, randomness)
+            config_goal = policy.generate_config(mech, goal_config)
+            pose_handle_world_init = util.Pose(*p.getLinkState(bb.bb_id, mech.handle_id)[:2])
 
-        # execute trajectory
-        cumu_motion, net_motion, pose_handle_world_final = \
-                gripper.execute_trajectory(traj, mech, policy.type, debug)
+            # calculate trajectory
+            pose_handle_base_world = mech.get_pose_handle_base_world()
+            traj = policy.generate_trajectory(pose_handle_base_world, config_goal, debug)
 
-        # save result data
-        policy_params = policy.get_policy_tuple()
-        mechanism_params = mech.get_mechanism_tuple()
-        results += [util.Result(policy_params, mechanism_params, net_motion, \
-                    cumu_motion, pose_handle_world_init, pose_handle_world_final, \
-                    config_goal, image_data, git_hash, randomness)]
+            # execute trajectory
+            cumu_motion, net_motion, pose_handle_world_final = \
+                    gripper.execute_trajectory(traj, mech, policy.type, debug)
 
-    p.disconnect()
+            # save result data
+            policy_params = policy.get_policy_tuple()
+            mechanism_params = mech.get_mechanism_tuple()
+            results += [util.Result(policy_params, mechanism_params, net_motion, \
+                        cumu_motion, pose_handle_world_init, pose_handle_world_final, \
+                        config_goal, image_data, git_hash, randomness)]
+
+        p.disconnect()
     return results
 
 results = []
-def generate_dataset(n_samples, viz, debug, git_hash, urdf_num, match_policies, \
+def generate_dataset(n_samples, n_bbs, viz, debug, git_hash, urdf_num, match_policies, \
                         randomness, goal_config, max_mech):
-    for i in range(n_samples):
-        sys.stdout.write("\rProcessing sample %i/%i" % (i+1, n_samples))
-        results.extend(generate_samples(viz, debug, max_mech, match_policies, randomness,
+    for i in range(n_bbs):
+        sys.stdout.write("\rProcessing % samples for busybox %i/%i" % (n_samples, i+1, n_bbs))
+        results.extend(generate_samples(n_samples, viz, debug, max_mech, match_policies, randomness,
                                     goal_config, None, git_hash, urdf_num))
     print()
 
@@ -56,7 +57,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--viz', action='store_true')
     parser.add_argument('--debug', action='store_true')
-    parser.add_argument('--n-samples', type=int, default=5) # number bbs to generate
+    parser.add_argument('--n-samples', type=int, default=1) # number samples per bb
+    parser.add_argument('--n-bbs', type=int, default=5) # number bbs to generate
     parser.add_argument('--max-mech', type=int, default=1) # mechanisms per bb
     parser.add_argument('--fname', type=str) # give filename
     # if running multiple gens, give then a urdf_num so the correct urdf is read from/written to
@@ -67,8 +69,8 @@ if __name__ == '__main__':
     parser.add_argument('--goal-config', type=float)
     args = parser.parse_args()
 
-    if args.debug:
-        import pdb; pdb.set_trace()
+    #if args.debug:
+    import pdb; pdb.set_trace()
 
     try:
         try:
@@ -79,7 +81,7 @@ if __name__ == '__main__':
         except:
             print('install gitpython to save git hash to results')
             git_hash = None
-        generate_dataset(args.n_samples, args.viz, args.debug, git_hash, args.urdf_num, \
+        generate_dataset(args.n_samples, args.n_bbs, args.viz, args.debug, git_hash, args.urdf_num, \
                         args.match_policies, args.randomness, args.goal_config, \
                         args.max_mech)
         if args.fname:
