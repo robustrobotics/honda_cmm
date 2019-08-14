67f8eaeed4ebc55dfed3015fb55f582d1f001ae2
Caris Moses <carism@mit.edu>
2019-08-14 17:01:05 -0400

    docker container can now be run as a pod with kubernetes and openstack

---
+++ run.sh
@@ -0,0 +1,7 @@
+./mc config host add csail $CSAIL_ENDPOINT $CSAIL_ACCESS $CSAIL_SECRET
+git clone https://${GITHUB_USERNAME}:${GITHUB_KEY}@github.com/robustrobotics/honda_cmm.git -b ${BRANCH:master} --single-branch
+cd honda_cmm
+/./mc cp csail/carism/small_set tmp/data1
+/./mc cp csail/carism/small_set tmp/data2
+python3 -m learning.train --batch-size 5 --hdim 5 --n-epochs 10 --use-cuda \
+        --data-fname tmp/data1 --data-fname2 tmp/data2 --mode ntrain --n-train 100
