7203617835fbe3022d50c05cee19b161030a8d55
Caris Moses <carism@mit.edu>
2019-08-14 17:15:10 -0400

    typo in bash

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ cd honda_cmm
 /./mc cp csail/carism/small_set tmp/data1
 /./mc cp csail/carism/small_set tmp/data2
 python3 -m learning.train --batch-size 5 --hdim 5 --n-epochs 10 --use-cuda \
-        --data-fname tmp/data1 --data-fname2 tmp/data2 --mode ntrain --n-train 100
+        --data-path tmp/data1 --data-path2 tmp/data2 --mode ntrain --n-train 100
