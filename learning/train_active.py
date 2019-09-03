1a7444cfa3f69217ca2ce7aa4331ab71e93d6ed9
Caris Moses <carism@mit.edu>
2019-09-03 13:26:42 -0400

    debug on crashing, just set num of BBs with args

--- learning/train_active.py
+++ learning/train_active.py
@@ -76,51 +76,58 @@ if __name__ == '__main__':
     parser.add_argument('--use-cuda', default=False, action='store_true')
     parser.add_argument('--debug', action='store_true')
     parser.add_argument('--image-encoder', type=str, default='spatial', choices=['spatial', 'cnn'])
-    parser.add_argument('--n-max', type=int, default=3000) # maximum number of robot interactions
+    parser.add_argument('--n-bbs', type=int, default=5000) # maximum number of robot interactions
     parser.add_argument('--data-type', default='active', choices=['active', 'random'])
     args = parser.parse_args()
 
     if args.debug:
         import pdb; pdb.set_trace()
 
-    # remove directories for tensorboard logs and torch model then remake
-    model_dir = './torch_models_prior/'
-    runs_dir = './runs_active'
-    dirs = [model_dir, runs_dir]
-    for dir in dirs:
-        if os.path.isdir(dir):
-            shutil.rmtree(dir)
+    try:
+        # remove directories for tensorboard logs and torch model then remake
+        model_dir = './torch_models_prior/'
+        runs_dir = './runs_active'
+        dirs = [model_dir, runs_dir]
+        for dir in dirs:
+            if os.path.isdir(dir):
+                shutil.rmtree(dir)
             os.makedirs(dir)
 
-    # want to generate the same sequence of BBs every time
-    np.random.seed(0)
-    model_path = model_dir + args.data_type + '.pt'
-    n_int_samples = 200 # num samples per bb
-    n_prior_samples = 200
-    n_bbs = int(args.n_max/n_int_samples)
-    dataset = []
-    writer = SummaryWriter(runs_dir)
-    for i in range(1, n_bbs+1):
-        print('BusyBox: ', i, '/', n_bbs)
-        rand_int = np.random.randint(100000)
-        np.random.seed(rand_int)
-        bb = BusyBox.generate_random_busybox(max_mech=1, mech_types=[Slider], urdf_tag=str(rand_int), debug=False)
-        data, prior_figs, final_figs = active_prior.generate_dataset(1, n_int_samples, n_prior_samples, False, False, str(rand_int), 1, False, 'random' == args.data_type, bb=bb, model_path=model_path, hdim=args.hdim)
-        dataset += data
-        parsed_data = parse_pickle_file(data=dataset)
-        plot_fname = args.data_type+'_'+str(i)
-        train_error, model = train_eval(args, parsed_data, plot_fname)
-        writer.add_scalar('Loss/train', train_error, i)
-        if prior_figs[0]:
-            writer.add_figure('Prior/'+str(i), prior_figs[0])
-        if final_figs[0]:
-            writer.add_figure('Final/'+str(i), final_figs[0])
-        path = model_dir + args.data_type + '.pt'
-        torch.save(model, path)
-        if i % args.plot_freq == 0:
-            save_model_path = dir + plot_fname + '.pt'
+        # want to generate the same sequence of BBs every time
+        np.random.seed(0)
+        model_path = model_dir + args.data_type + '.pt'
+        n_int_samples = 200 # num samples per bb
+        n_prior_samples = 100
+        viz_plot = False
+        dataset = []
+        writer = SummaryWriter(runs_dir)
+        for i in range(1, args.n_bbs+1):
+            print('BusyBox: ', i, '/', args.n_bbs)
+            rand_int = np.random.randint(100000)
+            bb = BusyBox.generate_random_busybox(max_mech=1, mech_types=[Slider], urdf_tag=str(rand_int), debug=False)
+            data, prior_figs, final_figs = active_prior.generate_dataset(1, n_int_samples, n_prior_samples, False, False, str(rand_int), 1, viz_plot, 'random' == args.data_type, bb=bb, model_path=model_path, hdim=args.hdim)
+            dataset += data
+            parsed_data = parse_pickle_file(data=dataset)
+            plot_fname = args.data_type+'_'+str(i)
+            train_error, model = train_eval(args, parsed_data, plot_fname)
+            writer.add_scalar('Loss/train', train_error, i)
+            if prior_figs[0]:
+                writer.add_figure('Prior/'+str(i), prior_figs[0])
+            if final_figs[0]:
+                writer.add_figure('Final/'+str(i), final_figs[0])
+            path = model_dir + args.data_type + '.pt'
             torch.save(model, path)
-            # TODO: visualize predicted competence
-            # writer figure to tensorboard at this frequency
+            if i % args.plot_freq == 0:
+                save_model_path = dir + plot_fname + '.pt'
+                torch.save(model, path)
+                # TODO: visualize predicted competence
+                # writer figure to tensorboard at this frequency
 
-    writer.close()
+        writer.close()
+    except:
+        # for post-mortem debugging since can't run module from command line in pdb.pm() mode
+        import traceback, pdb, sys
+        traceback.print_exc()
+        print('')
+        pdb.post_mortem()
+        sys.exit(1)
