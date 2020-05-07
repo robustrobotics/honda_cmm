Fix variance hyperparameters

learning/gp/explore_single_bb.py
@@ -210,9 +210,9 @@ class GPOptimizer(object):
                                                                 im=im,
                                                                 theta=theta)
         feats = (feats - self.learned_kernel['mu'])/self.learned_kernel['std']
-        with gpytorch.settings.fast_pred_var():
-            # pred = self.learned_kernel['likelihood'](self.learned_kernel['gp'](feats))
-            pred = self.learned_kernel['gp'](feats)
+        with gpytorch.settings.max_cg_iterations(10000), gpytorch.settings.max_preconditioner_size(200), gpytorch.settings.fast_computations(solves=False):
+            pred = self.learned_kernel['likelihood'](self.learned_kernel['gp'](feats))
+            #pred = self.learned_kernel['gp'](feats)
 
         pred_motion = pred.mean.cpu().detach().numpy()
         pred_std = pred.stddev.cpu().detach().numpy()
@@ -231,14 +231,13 @@ class GPOptimizer(object):
         # TODO: Extract features from the dataset.
         X_pol, Y = process_data(data, len(data))
         X_pol = torch.tensor(X_pol, dtype=torch.float32).cuda()
-        
         feats = self.learned_kernel['extractor'].forward_cached(policy_type='Revolute',
                                                                 im=self.cached_im,
                                                                 theta=X_pol)
         feats = (feats - self.learned_kernel['mu'])/self.learned_kernel['std']
-        with gpytorch.settings.fast_pred_var():
-            #pred = self.learned_kernel['likelihood'](self.learned_kernel['gp'](feats))
-            pred = self.learned_kernel['gp'](feats)
+        with gpytorch.settings.max_cg_iterations(10000), gpytorch.settings.max_preconditioner_size(200), gpytorch.settings.fast_computations(solves=False):
+            pred = self.learned_kernel['likelihood'](self.learned_kernel['gp'](feats))
+            #pred = self.learned_kernel['gp'](feats)
         pred_motion = pred.mean.cpu().detach().numpy()
         pred_std = pred.stddev.cpu().detach().numpy()
         if ucb:
@@ -383,13 +382,15 @@ class UCB_Interaction(object):
     def load_learned_kernel(self, nn_fname, gp_fname):
         print('Loading a learned kernel.')
         gp_state, train_xs, train_ys, mu, std = torch.load(gp_fname)
-        likelihood = gpytorch.likelihoods.GaussianLikelihood()
+        likelihood = gpytorch.likelihoods.GaussianLikelihood(noise_constraint=gpytorch.constraints.Interval(1e-8, 6.25e-6))
         gp = DistanceGP(train_x=train_xs,
                         train_y=train_ys,
                         likelihood=likelihood)
         extractor = FeatureExtractor(pretrained_nn_path=nn_fname)
         gp.load_state_dict(gp_state)
+        #gp.likelihood.noise = 4.82e-7
         gp.eval()
+        print(likelihood.noise)
 
         self.learned_kernel = {
             'gp': gp.cuda(),
@@ -400,6 +401,7 @@ class UCB_Interaction(object):
             'train_xs': train_xs,
             'train_ys': train_ys
         }
+        print(self.learned_kernel['likelihood'].noise)
         #self.learned_kernel['gp'](train_xs)
         #self.learned_kernel['gp'].set_train_data(inputs=self.learned_kernel['train_xs'],
         #                                         targets=self.learned_kernel['train_ys'],
@@ -501,9 +503,9 @@ class UCB_Interaction(object):
         xs, ys = self.learned_kernel['train_xs'], self.learned_kernel['train_ys']
         
         # Print updated prediction. 
-        with gpytorch.settings.fast_pred_var():
-            #pred = self.learned_kernel['likelihood'](self.learned_kernel['gp'](feats))
-            pred = self.learned_kernel['gp'](feats)
+        with gpytorch.settings.max_cg_iterations(10000), gpytorch.settings.max_preconditioner_size(200), gpytorch.settings.fast_computations(solves=False, covar_root_decomposition=False, log_prob=False):
+            pred = self.learned_kernel['likelihood'](self.learned_kernel['gp'](feats))
+        #pred = self.learned_kernel['gp'](feats)
         pred_motion = pred.mean.cpu().detach().numpy()
         pred_std = pred.stddev.cpu().detach().numpy()
         print('Before:', pred_motion, pred_std)
@@ -512,10 +514,11 @@ class UCB_Interaction(object):
         self.learned_kernel['gp'].set_train_data(inputs=self.learned_kernel['train_xs'],
                                                  targets=self.learned_kernel['train_ys'],
                                                  strict=False)
+        #self.learned_kernel['gp'] = self.learned_kernel['gp'].get_fantasy_model(feats, new_y)
         # Print updated prediction. 
-        with gpytorch.settings.fast_pred_var():
-            #pred = self.learned_kernel['likelihood'](self.learned_kernel['gp'](feats))
-            pred = self.learned_kernel['gp'](feats)
+        with gpytorch.settings.max_preconditioner_size(200), gpytorch.settings.max_cg_iterations(10000), gpytorch.settings.fast_computations(solves=False, covar_root_decomposition=False, log_prob=False):
+            pred = self.learned_kernel['likelihood'](self.learned_kernel['gp'](feats))
+        #pred = self.learned_kernel['gp'](feats)
         pred_motion = pred.mean.cpu().detach().numpy()
         pred_std = pred.stddev.cpu().detach().numpy()
         print('After:', pred_motion, pred_std)

learning/models/nn_with_kernel.py
@@ -8,7 +8,7 @@ from learning.modules.image_encoder import ImageEncoder as CNNEncoder
 from learning.models.nn_disp_pol_vis import DistanceRegressor
 from learning.dataloaders import setup_data_loaders, parse_pickle_file
 import gpytorch
-from gpytorch.kernels import GridInterpolationKernel, ScaleKernel, RBFKernel
+from gpytorch.kernels import GridInterpolationKernel, ScaleKernel, RBFKernel, MaternKernel
 from utils.util import load_model, read_from_file
 
 
@@ -45,28 +45,26 @@ class PretrainedDistanceGP(gpytorch.models.ExactGP):
         super(PretrainedDistanceGP, self).__init__(train_x, train_y, likelihood)
         num_gp_dims = 3
         self.mean_module = gpytorch.means.ConstantMean()
-        self.covar_module = ScaleKernel(RBFKernel(ard_num_dims=num_gp_dims, lengthscale_constraint=gpytorch.constraints.Interval(0.0001, 1.)), outputscale_constraint=gpytorch.constraints.Interval(0.00001, 1.))
-        pol_dim, im_dim, h_dim = 16, 32, 32
+        self.covar_module = ScaleKernel(RBFKernel(ard_num_dims=num_gp_dims, lengthscale_constraint=gpytorch.constraints.Interval(0.0001, 1.)), outputscale_constraint=gpytorch.constraints.Interval(1e-8, 1.e-6))
+        pol_dim, im_dim, h_dim = 8, 32,32
         
-        self.lin = nn.Linear(h_dim, num_gp_dims)
+        self.lin = nn.Linear(h_dim, h_dim)
+        self.lin2 = nn.Linear(h_dim, num_gp_dims)
         
     
     def embed_input(self, x): 
-        x = self.lin(x)
+        x = torch.tanh(self.lin(x))
+        x = self.lin2(x)
         return x
 
     def forward(self, x):
         x = self.embed_input(x)
 
         # The standardization shouldn't change at test time.
-        if self.training:
-            self.xmin = x.min(0)[0]
-            x = x - self.xmin
-            self.xmax = x.max(0)[0]
-            x = 2*(x/self.xmax)-1
-        else:
-            x = x - self.xmin
-            x = 2*(x/self.xmax)-1
+        self.xmin = x.min(0)[0]
+        x = x - self.xmin
+        self.xmax = x.max(0)[0]
+        x = 2*(x/self.xmax)-1
         mean_x = self.mean_module(x)
         covar_x = self.covar_module(x)
         return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
@@ -102,16 +100,18 @@ class FeatureExtractor(nn.Module):
 class DistanceGP(gpytorch.models.ExactGP):
     def __init__(self, train_x, train_y, likelihood):
         super(DistanceGP, self).__init__(train_x, train_y, likelihood)
-        num_gp_dims = 3
-        self.mean_module = gpytorch.means.ConstantMean()
-        self.covar_module = ScaleKernel(RBFKernel(ard_num_dims=num_gp_dims, lengthscale_constraint=gpytorch.constraints.Interval(0.0001, 1.)), outputscale_constraint=gpytorch.constraints.Interval(0.00001, 1.))
-        pol_dim, im_dim, h_dim = 16, 32, 8
+        num_gp_dims = 4
+        self.mean_module = gpytorch.means.ZeroMean()
+        self.covar_module = ScaleKernel(RBFKernel(ard_num_dims=num_gp_dims, 
+                                                  lengthscale_constraint=gpytorch.constraints.Interval(1.e-6, 1.)), 
+                                        outputscale_constraint=gpytorch.constraints.Interval(0.0225, 1.e-1))
+        pol_dim, im_dim, h_dim = 32, 32, 128
         self.pol_embed = nn.Linear(3, pol_dim)
         self.im_embed = nn.Linear(im_dim, pol_dim)
-        self.lin = nn.Linear(pol_dim, h_dim)
-        self.lin2 = nn.Linear(pol_dim+h_dim, h_dim)
-        self.lin3 = nn.Linear(pol_dim+h_dim, num_gp_dims)
-        
+        self.lin = nn.Linear(2*pol_dim, h_dim)
+        self.lin2 = nn.Linear(h_dim, h_dim)
+        self.lin3 = nn.Linear(h_dim, num_gp_dims)
+        self.dropout = nn.Dropout(0.2) 
         #self.cuda()
         #self.init_minmax(train_x)
     
@@ -126,31 +126,35 @@ class DistanceGP(gpytorch.models.ExactGP):
 
         # First embed the policy and image.
         pol = self.pol_embed(theta)
-        im = torch.sigmoid(self.im_embed(im))
+        im = self.im_embed(im)
         
+        embed = torch.tanh(torch.cat([pol, im], dim=1))
+        x = self.dropout(embed)
         # Use skip connections to encourage the kernel to pay attention to the policy.
-        x = pol+im
-        x = torch.sigmoid(self.lin(x))
-        #x = torch.cat([x, pol], dim=1)
-        #x = torch.sigmoid(self.lin2(x))
-        x = torch.cat([x, pol], dim=1)
+        x = torch.tanh(self.lin(embed))
+        x = self.dropout(x)
+        #x = torch.cat([x, embed], dim=1)
+        x = torch.tanh(self.lin2(x))
+        x = self.dropout(x)
+        #x = torch.cat([x, embed], dim=1)
         x = self.lin3(x)
         return x
+
 
     def forward(self, x):
         x = self.embed_input(x)
 
         # The standardization shouldn't change at test time.
-        if True:#self.training: # self.training:
-            # self.xmin = x.min(0)[0]
-            x = x - x.min(0)[0]
-            # self.xmax = x.max(0)[0]
-            x = 2*(x/x.max(0)[0])-1
-        else:
-            x = x - self.xmin
-            x = 2*(x/self.xmax)-1
+        x = x - x.min(0)[0]
+        x = 2*(x/x.max(0)[0])-1
+        
         mean_x = self.mean_module(x)
         covar_x = self.covar_module(x)
+        #import pickle
+        #with open('debug.pickle', 'wb') as handle:
+        #    pickle.dump([mean_x.clone().cpu().detach().numpy(), 
+        #                 covar_x.clone().cpu().detach().numpy()], handle)
+        #print(mean_x.shape, covar_x.shape)
         return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 
 
@@ -201,7 +205,7 @@ if __name__ == '__main__':
     val_results = [item for sublist in val_results for item in sublist]
     val_data = parse_pickle_file(val_results)
     
-    train_set, _, _ = setup_data_loaders(data=data, batch_size=16)
+    train_set = setup_data_loaders(data=data, batch_size=16, single_set=True)
     val_set = setup_data_loaders(data=val_data, batch_size=16, single_set=True)
     
     # Extract features for this dataset.
@@ -220,9 +224,12 @@ if __name__ == '__main__':
     val_x = (val_x - mu)/std
     print('Data Size:', train_x.shape, train_y.shape)
     print(train_x.size(0), train_y.size(0))
-    #noise = torch.ones(1)*2e-5
-    #likelihood = gpytorch.likelihoods.FixedNoiseGaussianLikelihood(noise)
-    likelihood = gpytorch.likelihoods.GaussianLikelihood(noise_constraint=gpytorch.constraints.GreaterThan(9e-6))  # 0.0001, 4e-6
+    print(train_y)
+    #mu_y, std_y = torch.mean(train_y), torch.std(train_y)
+    #train_y = (train_y - mu_y)/std_y
+    # noise = torch.ones(1)*1e-7
+    # likelihood = gpytorch.likelihoods.FixedNoiseGaussianLikelihood(noise)
+    likelihood = gpytorch.likelihoods.GaussianLikelihood(noise_constraint=gpytorch.constraints.Interval(1e-8, 6.25e-6))  # 0.0001, 4e-6
     gp = DistanceGP(train_x=train_xs,
                     train_y=train_y,
                     likelihood=likelihood)
@@ -231,7 +238,7 @@ if __name__ == '__main__':
         gp = gp.cuda()
     gp.train()
     likelihood.train()
-    optimizer = torch.optim.Adam([{'params': gp.parameters()}], lr=0.1)
+    optimizer = torch.optim.Adam([{'params': gp.parameters()}], lr=0.01)
     #optimizer = torch.optim.Adam([{'params':gp.covar_module.parameters(), 'lr':0.1},
     #                              {'params':gp.lin.parameters()},
     #                              {'params':gp.lin3.parameters()},
@@ -241,28 +248,31 @@ if __name__ == '__main__':
     #                              {'params':gp.likelihood.parameters(), 'lr':0.1}])
     mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, gp)
     val_loss_fn = torch.nn.MSELoss()
-    with gpytorch.settings.max_preconditioner_size(200), gpytorch.settings.max_cg_iterations(10000), gpytorch.settings.fast_computations(log_prob=False, solves=True):
-        for tx in range(2000):
+    best = 1000
+    with gpytorch.settings.max_preconditioner_size(200), gpytorch.settings.max_cg_iterations(10000), gpytorch.settings.fast_computations(log_prob=False, solves=False, covar_root_decomposition=False):
+        for tx in range(6000):
             optimizer.zero_grad()
             output = gp(train_xs)
             loss = -mll(output, train_y)
             loss.backward()
             optimizer.step()
 
-            if tx % 50 == 0:
-                print(loss.item(), gp.likelihood.noise.item(), gp.covar_module.base_kernel.lengthscale, gp.covar_module.outputscale.item())
-                gp.eval()
-                likelihood.eval()
-                output = gp(val_x)
-                val_loss = val_loss_fn(output.mean, val_y)
-                output = gp(train_xs)
-                train_loss = val_loss_fn(output.mean, train_y)
-                print('Train Loss:', train_loss.item())
-                print('Val Loss:', val_loss.item())
-                gp.train()
-                likelihood.train()
-
-        torch.save((gp.state_dict(), train_xs, train_y, mu, std), args.save_path)
+            if tx % 100 == 0:
+                print(tx, loss.item(), gp.likelihood.noise.item(), gp.covar_module.base_kernel.lengthscale, gp.covar_module.outputscale.item())
+                #gp.eval()
+                #likelihood.eval()
+                #output = gp(val_x)
+                #val_loss = val_loss_fn(output.mean, val_y)
+                #output = gp(train_xs)
+                #train_loss = val_loss_fn(output.mean, train_y)
+                #print('Train Loss:', train_loss.item())
+                #print('Val Loss:', val_loss.item())
+                #gp.train()
+                #likelihood.train()
+                
+                #if loss.item() < best:
+            best = loss.item()
+            torch.save((gp.state_dict(), train_xs, train_y, mu, std), args.save_path)
     gp.eval()
     print('Val Predictions')
     for ix in range(0, 20):#val_x.shape[0]):
@@ -273,7 +283,7 @@ if __name__ == '__main__':
         true = val_y[ix].item()
         
         p = pred.mean.cpu().detach().numpy()[0]
-        if p < lower or p > upper:
+        if true < lower or true > upper:
             print('INCORRECT:', pred.mean.cpu().detach().numpy(), 
                 (lower[0], upper[0]), upper - lower,
                 val_y[ix])

utils/plot_uncertainty.py
@@ -46,8 +46,9 @@ def get_callback(gp_model, likelihood, feature_extractor, p_mu, p_std):
                                                  policy_tensor)
             features = (features - p_mu)/p_std
 
-            #print(gp_model.get_gp_inputs(features))
-            with gpytorch.settings.fast_pred_var():
+            #print(gp_model.embed_input(features))
+            with gpytorch.settings.max_preconditioner_size(200), gpytorch.settings.max_cg_iterations(10000),  gpytorch.settings.fast_computations(log_prob=False, solves=False, covar_root_decomposition=False):
+            #with gpytorch.settings.fast_pred_var():
                 pred = gp_model(features)
             pred_motion_float = pred.mean.cpu().detach().numpy()[0]
             pred_std = pred.stddev.cpu().detach().numpy()[0]
@@ -99,7 +100,7 @@ if __name__ == '__main__':
 
     # Load the GP and Feature Extractor.
     gp_state, train_xs, train_ys, mu, std = torch.load(args.gp_path)
-    likelihood = gpytorch.likelihoods.GaussianLikelihood()
+    likelihood = gpytorch.likelihoods.GaussianLikelihood(noise_constraint=gpytorch.constraints.Interval(1e-8, 6.25e-6))
     gp = DistanceGP(train_x=train_xs,
                     train_y=train_ys,
                     likelihood=likelihood)
@@ -110,9 +111,12 @@ if __name__ == '__main__':
         gp.cuda()
     gp.load_state_dict(gp_state)
     print(gp.covar_module.base_kernel.lengthscale)
-
+    print(gp.likelihood.noise)
     # Load validation dataset.
     raw_results = read_from_file('/home/mnosew/workspace/honda_cmm/data/doors_gpucb_100L_100M_set0.pickle')
+    #raw_results = read_from_file('100_eval_doors.pickle')
+    #val_results = [bb[::] for bb in raw_results]
+    #val_results = [item for sublist in val_results for item in sublist]
     val_results = [bb[::] for bb in raw_results[82:]]
     val_results = [item for sublist in val_results for item in sublist]
     val_data = parse_pickle_file(val_results)
@@ -121,26 +125,30 @@ if __name__ == '__main__':
     val_x = (val_x - mu)/std
     
     gp.eval()
+    likelihood.eval()
     print('Val Predictions')
-    for ix in range(0, 20):#val_x.shape[0]):
-        pred = likelihood(gp(val_x[ix:ix+1, :]))
-        lower, upper = pred.confidence_region()
-        lower = lower.cpu().detach().numpy()
-        upper = upper.cpu().detach().numpy()
-        true = val_y[ix].item()
-        
-        p = pred.mean.cpu().detach().numpy()[0]
-        if p < lower or p > upper:
-            print('INCORRECT:', pred.mean.cpu().detach().numpy(), 
-                (lower[0], upper[0]), upper - lower,
-                val_y[ix])
-        elif upper[0]-lower[0] > 0.05:
-            print('UNSURE:', pred.mean.cpu().detach().numpy(), 
-                (lower[0], upper[0]), upper - lower,
-                val_y[ix])
-        else:
-            print('CORRECT')
+    with gpytorch.settings.max_preconditioner_size(200), gpytorch.settings.max_cg_iterations(10000), gpytorch.settings.fast_computations(log_prob=False, solves=False, covar_root_decomposition=False):
+        for ix in range(0, 20):#val_x.shape[0]):
+            #print(gp.likelihood.noise)
+            pred = likelihood(gp(val_x[ix:ix+1, :]))
+            lower, upper = pred.confidence_region()
+            lower = lower.cpu().detach().numpy()
+            upper = upper.cpu().detach().numpy()
+            true = val_y[ix].item()
+            
+            p = pred.mean.cpu().detach().numpy()[0]
+            if true < lower or true > upper:
+                print('INCORRECT:', pred.mean.cpu().detach().numpy(), 
+                    (lower[0], upper[0]), upper - lower,
+                    val_y[ix])
+            elif upper[0]-lower[0] > 0.05:
+                print('UNSURE:', pred.mean.cpu().detach().numpy(), 
+                    (lower[0], upper[0]), upper - lower,
+                    val_y[ix])
+            else:
+                print('CORRECT')
     
+        #print('COMP:',(upper-lower)/4., pred.stddev.cpu().detach().numpy())
     #viz_interaction()
     viz_3d_plots(xs=[],
                  callback=get_callback(gp, likelihood, extractor, mu, std),