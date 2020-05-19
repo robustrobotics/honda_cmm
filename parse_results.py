a442f3c644cd90334823810e27a54144574a1dbc
Cindy Zhang <cindyyz@mit.edu>
2020-05-19 14:47:53 -0700

    Added data and plots

--- parse_results.py
+++ parse_results.py
@@ -86,6 +86,8 @@ with open('results_b2.txt') as f:
   lineList2 = f.readlines()
 with open('results_b3.txt') as f:
   lineList3 = f.readlines()
+with open('results_b4.txt') as f:
+  lineList4 = f.readlines()
 i = 0
 while i < 98:
 	line = lineList1[i][:-1]
@@ -122,19 +124,32 @@ while i < 98:
 	base_val_y.append(y)
 	i += 2
 
+i = 0
+while i < 98:
+	line = lineList4[i][:-1]
+	elem = line.split()
+	y = float(elem[-1])
+	base_train_y.append(y)
+	line = lineList4[i+1][:-1]
+	elem = line.split()
+	y = float(elem[-1])
+	base_val_y.append(y)
+	i += 2
+
 x = np.array(train_x)
-y = np.array(train_y).reshape(5,50)
+y = np.array(val_y).reshape(5,50)
 err_y = np.std(y, axis = 0).ravel()
 mean_y = np.mean(y, axis = 0).ravel()
-plt.plot(x, mean_y, color='#b53b33')
+plt.plot(x, mean_y, color='#b53b33', label = 'with buffer')
 plt.fill_between(x, mean_y-err_y, mean_y+err_y,
     alpha=0.5, edgecolor='#b53b33', facecolor='#ed908a')
 
 x = np.array(train_x[:49])
-base_y = np.array(base_train_y).reshape(3,49)
+base_y = np.array(base_val_y).reshape(4,49)
 berr_y = np.std(base_y, axis = 0).ravel()
 bmean_y = np.mean(base_y, axis = 0).ravel()
-plt.plot(x, bmean_y, color='#1B2ACC')
+plt.plot(x, bmean_y, color='#1B2ACC', label = 'without buffer')
 plt.fill_between(x, bmean_y-berr_y, bmean_y+berr_y,
     alpha=0.2, edgecolor='#1B2ACC', facecolor='#089FFF')
-plt.show()
+plt.legend()
+plt.savefig('val_loss.png')
