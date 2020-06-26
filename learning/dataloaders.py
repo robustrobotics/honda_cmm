Fix numerical issues with GP and add diagnostic tests for training.

learning/dataloaders.py
@@ -78,6 +78,7 @@ class PolicyDataset(Dataset):
         self.items = items
 
         self.tensors = [torch.tensor(item['params']) for item in items]
+        self.mechs = [torch.tensor(item['mechs']) for item in items]
         self.ys = [torch.tensor([item['y']]) for item in items]
 
         downsample = transforms.Compose([transforms.ToPILImage(),
@@ -98,7 +99,7 @@ class PolicyDataset(Dataset):
         # imshow(torchvision.utils.make_grid(self.downsampled_images[0:10]))
 
     def __getitem__(self, index):
-        return self.items[index]['type'], self.tensors[index], self.images[index], self.ys[index], self.downsampled_images[index]
+        return self.items[index]['type'], self.tensors[index], self.images[index], self.ys[index], self.mechs[index]#self.downsampled_images[index]
 
     def __len__(self):
         return len(self.items)
@@ -119,9 +120,17 @@ def parse_pickle_file(results):
             if entry.policy_params.param_data[param].varied:
                 policy_params.append(entry.policy_params.params[param])
 
+        mech_params = [0., 0., 0.]
+        mech_params[0] = entry.mechanism_params.params.door_size[0]
+        if entry.mechanism_params.params.flipped == 1:
+            mech_params[1] = 1.
+        else:
+            mech_params[2] = 1.
+
         parsed_data.append({
             'type': entry.policy_params.type,
             'params': policy_params,
+            'mechs': mech_params,
             'image': entry.image_data,
             'y': entry.net_motion,
         })
@@ -150,8 +159,8 @@ def setup_data_loaders(data, batch_size=128, use_cuda=True, small_train=0, singl
     if single_set:
         set = PolicyDataset(data)
         loader = torch.utils.data.DataLoader(dataset=set,
-                                                   batch_sampler=CustomSampler(set.items, batch_size),
-                                                   **kwargs)
+                                             batch_sampler=CustomSampler(set.items, batch_size),
+                                             **kwargs)
         return loader
     else:
         # Create datasplits.

learning/gp/evaluate_models.py
@@ -47,16 +47,16 @@ def evaluate_models(n_interactions, n_bbs, args, use_cuda=False):
                 all_model_test_regrets.append(regret)
                 if args.debug:
                     print('Test Regret   :', regret)
+            
+                all_L_results[model] = all_model_test_regrets
+                if len(models) > 0:
+                    all_results[L] = all_L_results
+                util.write_to_file('regret_results_%s_%dT_%dN.pickle' % (args.type, n_interactions, n_bbs),
+                                all_results,
+                                verbose=True)
             if args.debug:
                 print('Results')
-                # print('Average Regret:', np.mean(avg_regrets))
                 print('Final Regret  :', np.mean(all_model_test_regrets))
-            all_L_results[model] = all_model_test_regrets
-        if len(models) > 0:
-            all_results[L] = all_L_results
-    util.write_to_file('regret_results_%s_%dT_%dN.pickle' % (args.type, n_interactions, n_bbs),
-                       all_results,
-                       verbose=True)
 
 # regret_results file the same as above, but instead of lists of regret values
 # for each BB and model, it is a list of the number of steps it took GPUCB to

learning/gp/explore_single_bb.py
@@ -26,12 +26,12 @@ from learning.gp.viz_doors import viz_3d_plots
 from learning.gp.viz_polar_plots import viz_circles
 import time
 import gpytorch
-from learning.models.nn_with_kernel import FeatureExtractor, DistanceGP
+from learning.models.nn_with_kernel import FeatureExtractor, DistanceGP, ProductDistanceGP
 from learning.dataloaders import setup_data_loaders, parse_pickle_file
 from utils.plot_uncertainty import get_callback
 
 
-BETA = 4
+BETA = 2
 
 # takes in a policy and returns and optimization x and the variable bounds
 def get_x_and_bounds_from_tuple(policy_params):
@@ -382,8 +382,8 @@ class UCB_Interaction(object):
     def load_learned_kernel(self, nn_fname, gp_fname):
         print('Loading a learned kernel.')
         gp_state, train_xs, train_ys, mu, std = torch.load(gp_fname)
-        likelihood = gpytorch.likelihoods.GaussianLikelihood(noise_constraint=gpytorch.constraints.Interval(1e-8, 6.25e-6))
-        gp = DistanceGP(train_x=train_xs,
+        likelihood = gpytorch.likelihoods.GaussianLikelihood(noise_constraint=gpytorch.constraints.Interval(1e-8, 1e-6))
+        gp = ProductDistanceGP(train_x=train_xs,
                         train_y=train_ys,
                         likelihood=likelihood)
         extractor = FeatureExtractor(pretrained_nn_path=nn_fname)
@@ -396,8 +396,8 @@ class UCB_Interaction(object):
             'gp': gp.cuda(),
             'likelihood': likelihood.cuda(),
             'extractor': extractor.cuda(),
-            'mu': mu.cuda(),
-            'std': std.cuda(),
+            'mu': 0.,#mu.cuda(),
+            'std': 1.,#std.cuda(),
             'train_xs': train_xs,
             'train_ys': train_ys
         }
@@ -595,6 +595,7 @@ def create_single_bb_gpucb_dataset(bb_result, nn_fname, plot, args, bb_i,
     viz = False
     debug = False
     use_gripper = False
+    regrets = []
     # interact with BB
     bb = BusyBox.bb_from_result(bb_result, urdf_num=args.urdf_num)
     mech = bb._mechanisms[0]
@@ -606,54 +607,27 @@ def create_single_bb_gpucb_dataset(bb_result, nn_fname, plot, args, bb_i,
         gripper.reset(sampler.mech)
         if args.debug:
             sys.stdout.write('\rProcessing sample %i' % ix)
-        # if we are done sampling n_interactions OR we need to get regrNoner
-        # each interaction
-        if ((not n_interactions is None) and ix==n_interactions) or \
-            (not success_regret is None):
+        
+        # Always evaluate at each step.
+        regret, start_x, stop_x, policy_type = test_model(sampler, args, gripper=None)
+        gripper.reset(mech)
+        print('Best x:', stop_x)
+        print('Current regret', regret)
+        opt_points = (policy_type, [(start_x, 'g'), (stop_x, 'r')])
+        sample_points = {'Prismatic': [(sample, 'k') for sample in sampler.xs['Prismatic']],
+                        'Revolute': [(sample, 'k') for sample in sampler.xs['Revolute']]}
+        regrets.append(regret)
 
-            regret, start_x, stop_x, policy_type = test_model(sampler, args, gripper=None)
-            gripper.reset(mech)
-            print('Best x:', stop_x)
-            print('Current regret', regret)
-            opt_points = (policy_type, [(start_x, 'g'), (stop_x, 'r')])
-            sample_points = {'Prismatic': [(sample, 'k') for sample in sampler.xs['Prismatic']],
-                            'Revolute': [(sample, 'k') for sample in sampler.xs['Revolute']]}
-
-            # if done sampling n_interactions
-            if (not n_interactions is None) and ix==n_interactions:
-                if plot:
-                    viz_circles(util.GP_PLOT,
-                                image_data,
-                                mech,
-                                BETA,
-                                sample_points=sample_points,
-                                opt_points=opt_points,
-                                gps=sampler.gps,
-                                nn=sampler.nn,
-                                bb_i=bb_i,
-                                plot_dir_prefix=plot_dir_prefix)
-                    plt.show()
-                    input('Enter to close')
-                    plt.close()
-                if ret_regret:
-                    return dataset, sampler.gps, regret
-                else:
-                    return dataset, sampler.gps
-            # if got successful interaction or timeout
-            elif (not success_regret is None) and \
-                            ((regret < success_regret) or (ix >= 100)):
-                if plot:
-                    viz_circles(util.GP_PLOT,
-                                image_data,
-                                mech,
-                                BETA,
-                                sample_points=sample_points,
-                                opt_points=opt_points,
-                                gps=sampler.gps,
-                                nn=sampler.nn,
-                                bb_i=bb_i,
-                                plot_dir_prefix=plot_dir_prefix)
-                return dataset, sampler.gps, ix
+        # if done sampling n_interactions
+        if (not n_interactions is None) and ix==n_interactions:
+            if ret_regret:
+                return dataset, sampler.gps, regrets
+            else:
+                return dataset, sampler.gps
+        # if got successful interaction or timeout
+        elif (not success_regret is None) and \
+                        ((regret < success_regret) or (ix >= 100)):
+            return dataset, sampler.gps, ix
 
         # sample a policy
         image_data, gripper = setup_env(bb, False, debug, use_gripper)

learning/models/nn_with_kernel.py
@@ -8,9 +8,10 @@ from learning.modules.image_encoder import ImageEncoder as CNNEncoder
 from learning.models.nn_disp_pol_vis import DistanceRegressor
 from learning.dataloaders import setup_data_loaders, parse_pickle_file
 import gpytorch
-from gpytorch.kernels import GridInterpolationKernel, ScaleKernel, RBFKernel, MaternKernel
+from gpytorch.kernels import ScaleKernel, RBFKernel
+from gpytorch.constraints import Interval
 from utils.util import load_model, read_from_file
-
+import numpy as np
 
 class PretrainedFeatureExtractor(nn.Module):
     def __init__(self, pretrained_nn_path):
@@ -69,20 +70,31 @@ class PretrainedDistanceGP(gpytorch.models.ExactGP):
         covar_x = self.covar_module(x)
         return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 
+
 class FeatureExtractor(nn.Module):
     def __init__(self, pretrained_nn_path):
         super(FeatureExtractor, self).__init__()
         self.pretrained_model = load_model(model_fname=pretrained_nn_path,
                                            hdim=16)
         
-    def forward(self, policy_type, im, theta):
+    def forward(self, policy_type, im, mech, theta, mech_encoding='params'):
         policy_type = 1
         if policy_type == 0:
             policy_type = 'Prismatic'
         else:
             policy_type = 'Revolute'
-        pol = self.pretrained_model.policy_modules[policy_type].forward(theta)
-        im, points = self.pretrained_model.image_module(im)
+        
+        #pol = self.pretrained_model.policy_modules[policy_type].forward(theta)
+        # _, points = self.pretrained_model.image_module(im)
+
+        if mech_encoding == 'img':
+            bs, c, h, w = im.shape
+            # Downsample
+            im = im[:, :, ::2, ::2]
+            im = im.reshape(bs, -1)
+        else:
+            im = mech
+        
         x = torch.cat([im, theta], dim=1)
         #x = F.relu(self.pretrained_model.fc1(x))
         #x = F.relu(self.pretrained_model.fc2(x))
@@ -96,6 +108,128 @@ class FeatureExtractor(nn.Module):
         #x = F.relu(self.pretrained_model.fc2(x))
         return x
 
+class PolPredictor(nn.Module):
+    def __init__(self, gp):
+        super(PolPredictor, self).__init__()
+        self.net = gp.pol_net
+        self.lin = nn.Linear(gp.gp_action_dims, 1)
+
+    def forward(self, x):
+        x = self.net(x)
+        return self.lin(x)
+
+class ImPredictor(nn.Module):
+    def __init__(self, gp):
+        super(ImPredictor, self).__init__()
+        self.net = gp.im_net
+        self.lin = nn.Linear(gp.gp_im_dims, 1)
+
+    def forward(self, x):
+        x = self.net(x)
+        return self.lin(x)
+        
+
+class ProductDistanceGP(gpytorch.models.ExactGP):
+    def __init__(self, train_x, train_y, likelihood, mech_encoding='params'):
+        super(ProductDistanceGP, self).__init__(train_x, train_y, likelihood)
+        self.gp_action_dims, self.gp_im_dims = 3, 3
+        
+
+        self.mean_module = gpytorch.means.ZeroMean()
+        self.covar_module = ScaleKernel(RBFKernel(ard_num_dims=self.gp_action_dims+self.gp_im_dims, 
+        ),#lengthscale_constraint=Interval(1.e-6, 1.0)), # 1.e-6, .25
+                                        outputscale_constraint=Interval(0.01, 1.e-1)) # 0.01, 1.e-1 # 0.0225, 0.1 
+        pol_dim, im_dim, pol_h_dim, im_h_dim = 3, 3, 64, 64 # 128
+
+        pol_dropout = 0.
+        im_dropout = 0.
+
+        self.pol_net = nn.Sequential(*[nn.Linear(pol_dim, pol_h_dim),
+                                      nn.ReLU(),
+                                      nn.Dropout(pol_dropout),
+                                      nn.Linear(pol_h_dim, pol_h_dim),
+                                      nn.ReLU(),
+                                      nn.Dropout(pol_dropout),
+                                      nn.Linear(pol_h_dim, self.gp_action_dims),
+                                      nn.Tanh()])
+        if mech_encoding == 'params':
+            self.im_net = nn.Sequential(*[nn.Linear(im_dim, im_h_dim),
+                                        nn.ReLU(),
+                                        nn.Dropout(im_dropout),
+                                        nn.Linear(im_h_dim, im_h_dim),
+                                        nn.ReLU(),
+                                        nn.Dropout(im_dropout),
+                                        nn.Linear(im_h_dim, im_h_dim),
+                                        nn.ReLU(),
+                                        nn.Dropout(im_dropout),
+                                        nn.Linear(im_h_dim, self.gp_im_dims)])
+        elif mech_encoding == 'img':
+            self.c, self.h, self.w = 3, 59, 58
+            kernel_size, padding = 3, 1
+            self.conv1 = nn.Conv2d(in_channels=im_dim, out_channels=im_h_dim, kernel_size=kernel_size, padding=padding)
+            self.relu = nn.ReLU()
+            self.tanh = nn.Tanh()
+            self.maxpool = nn.MaxPool2d(kernel_size=2)
+            self.conv2 = nn.Conv2d(in_channels=im_h_dim, out_channels=im_h_dim, kernel_size=kernel_size, padding=padding)
+            self.conv3 = nn.Conv2d(in_channels=im_h_dim, out_channels=im_h_dim, kernel_size=kernel_size, padding=padding)
+            self.conv4 = nn.Conv2d(in_channels=im_h_dim, out_channels=im_h_dim, kernel_size=kernel_size, padding=padding)
+
+            self.flatten = nn.Flatten()
+            self.lin1 = nn.Linear(9*im_h_dim, im_h_dim)
+            self.lin2 = nn.Linear(im_h_dim, self.gp_im_dims)
+            self.dropout = nn.Dropout(0.)
+
+            self.im_net = self.im_net2
+
+    
+        # Seems to fix an issue where the embeddings of two points get very close causing numerical issues.
+        # Could look into this noise issue a bit more later.
+        
+    def im_net2(self, im):
+        im = im.view(-1, self.c, self.h, self.w)
+        im = self.conv1(im)
+        im = self.dropout(im)
+        im = self.relu(im)
+        im = self.maxpool(im)
+        im = self.conv2(im)
+        im = self.dropout(im)
+        im = self.relu(im)
+        im = self.maxpool(im)
+        im = self.conv3(im)
+        im = self.dropout(im)
+        im = self.relu(im)
+        im = self.maxpool(im)
+        im = self.conv4(im)
+        im = self.dropout(im)
+        im = self.relu(im)
+        im = self.maxpool(im)
+        im = self.flatten(im)
+        im = self.lin1(im)
+        im = self.dropout(im)
+        im = self.relu(im)
+        return self.tanh(self.lin2(im))
+
+    def embed_input(self, x): 
+        theta, im = x[:, -3:], x[:,:-3]
+
+        # First embed the policy.
+        pol = self.pol_net(theta)
+        im = self.im_net(im)
+        
+        x = torch.cat([pol, im], dim=1)
+        return x
+
+
+    def forward(self, x):
+        x = self.embed_input(x)
+
+        # The standardization shouldn't change at test time.
+        #x = x - x.min(0)[0]
+        #x = 2*(x/x.max(0)[0])-1
+        
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 
 class DistanceGP(gpytorch.models.ExactGP):
     def __init__(self, train_x, train_y, likelihood):
@@ -172,21 +306,81 @@ def convert_training_data(train_set):
 
     return xs, ys
 
-def extract_feature_dataset(dataset, extractor, use_cuda=False):
+def extract_feature_dataset(dataset, extractor, use_cuda=False, mech_encoding='params'):
     """ First extract the features from the dataset."""
     features, ys = [], []
-    for policy_type, theta, im, y, _ in dataset:
+    for policy_type, theta, im, y, mech in dataset:
         if use_cuda:
             theta = theta.cuda()
             im = im.cuda()
             y = y.cuda()
-        feats = extractor(policy_type, im, theta) 
+            mech = mech.cuda()
+        feats = extractor(policy_type, im, mech, theta, mech_encoding=mech_encoding) 
         features.append(feats.detach())
         ys.append(y)
     return torch.cat(features, dim=0), torch.cat(ys, dim=0).squeeze()
 
+def pretrain_modules(gp, train_x, train_y, epochs=5):
+    print('Pretraining...')
+    pol_net = PolPredictor(gp)
+    pol_net = pol_net.cuda()
+    pol_optim = torch.optim.Adam(pol_net.parameters())
+    pol_loss_fn = torch.nn.MSELoss()
+
+    im_net = ImPredictor(gp)
+    im_net = im_net.cuda()
+    im_optim = torch.optim.Adam(im_net.parameters())
+    im_loss_fn = torch.nn.MSELoss()
+    
+    for tx in range(epochs):
+        pol_losses = []
+        im_losses = []
+        for ix in range(train_x.shape[0]):
+            pol_optim.zero_grad()
+            pol_pred = pol_net(train_x[ix:ix+1, -3:])
+            pol_loss = pol_loss_fn(pol_pred, train_y[ix:ix+1])
+            pol_loss.backward()
+            pol_optim.step()
+            pol_losses.append(pol_loss.item())
+
+            im_optim.zero_grad()
+            im_pred = im_net(train_x[ix:ix+1, :-3])
+            im_loss = im_loss_fn(im_pred, train_y[ix:ix+1])
+            im_loss.backward()
+            im_optim.step()
+            im_losses.append(im_loss.item())
+        print('Loss (%d):' % tx, np.mean(pol_losses), np.mean(im_losses))
+    
+def remove_duplicates(results):
+    valid = []
+    print('Removing duplicates...')
+
+    found = 0
+    for bb_result in results:
+        valid.append([])
+        added = []
+        for entry in bb_result:
+            policy_params = []
+            for param in entry.policy_params.param_data:
+                if entry.policy_params.param_data[param].varied:
+                    policy_params.append(entry.policy_params.params[param])
+            policy_params = np.array(policy_params)
+            # Check if policy_params have already been added.
+            duplicate = False
+            for pol in added:
+                if np.linalg.norm(pol-policy_params) < 0.01:
+                    duplicate = True
+            if not duplicate:
+                added.append(policy_params)
+                valid[-1].append(entry)
+            else:
+                found += 1
+    print('Duplicates Found:', found)
+    return valid
+
 
 CUDA = True
+MECH_TYPE = 'img'
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--save-path', default='')
@@ -198,11 +392,13 @@ if __name__ == '__main__':
     #  Load dataset.
     raw_results = read_from_file('/home/mnosew/workspace/honda_cmm/data/doors_gpucb_100L_100M_set0.pickle')
     results = [bb[::] for bb in raw_results[:args.L]]  
-    results = [item for sublist in results for item in sublist][::]
+    results = remove_duplicates(results)
+    results = [item for sublist in results for item in sublist][::2]
     data = parse_pickle_file(results)
     
     val_results = [bb[::] for bb in raw_results[81:]]
     val_results = [item for sublist in val_results for item in sublist]
+    # print(val_results[0].mechanism_params)
     val_data = parse_pickle_file(val_results)
     
     train_set = setup_data_loaders(data=data, batch_size=16, single_set=True)
@@ -213,44 +409,42 @@ if __name__ == '__main__':
     extractor = FeatureExtractor(pretrained_nn_path=args.pretrained_nn)
     if CUDA:
         extractor.cuda()
-    train_x, train_y = extract_feature_dataset(train_set, extractor, use_cuda=CUDA)
+    train_x, train_y = extract_feature_dataset(train_set, extractor, use_cuda=CUDA, mech_encoding=MECH_TYPE)
+
     mu = torch.mean(train_x, dim=0, keepdims=True)
     std = torch.std(train_x, dim=0, keepdims=True)+1e-3
     print(std)
     train_xs = ((train_x-mu)/std).detach().clone()
     print('Features Extracted')
     print(train_x.shape, train_y.shape)
-    val_x, val_y = extract_feature_dataset(val_set, extractor, use_cuda=CUDA)
+    val_x, val_y = extract_feature_dataset(val_set, extractor, use_cuda=CUDA, mech_encoding=MECH_TYPE)
     val_x = (val_x - mu)/std
     print('Data Size:', train_x.shape, train_y.shape)
-    print(train_x.size(0), train_y.size(0))
-    print(train_y)
-    #mu_y, std_y = torch.mean(train_y), torch.std(train_y)
-    #train_y = (train_y - mu_y)/std_y
-    # noise = torch.ones(1)*1e-7
-    # likelihood = gpytorch.likelihoods.FixedNoiseGaussianLikelihood(noise)
-    likelihood = gpytorch.likelihoods.GaussianLikelihood(noise_constraint=gpytorch.constraints.Interval(1e-8, 6.25e-6))  # 0.0001, 4e-6
-    gp = DistanceGP(train_x=train_xs,
-                    train_y=train_y,
-                    likelihood=likelihood)
+
+    likelihood = gpytorch.likelihoods.GaussianLikelihood(noise_constraint=Interval(1e-8, 6.25e-6))  # 6.25e-6
+    gp = ProductDistanceGP(train_x=train_xs,
+                           train_y=train_y,
+                           likelihood=likelihood,
+                           mech_encoding=MECH_TYPE)
     if CUDA:
         likelihood = likelihood.cuda()
         gp = gp.cuda()
     gp.train()
     likelihood.train()
-    optimizer = torch.optim.Adam([{'params': gp.parameters()}], lr=0.01)
-    #optimizer = torch.optim.Adam([{'params':gp.covar_module.parameters(), 'lr':0.1},
-    #                              {'params':gp.lin.parameters()},
-    #                              {'params':gp.lin3.parameters()},
-    #                              {'params':gp.lin2.parameters()},
-    #                              {'params':gp.im_embed.parameters()},
-    #                              {'params':gp.pol_embed.parameters()},
-    #                              {'params':gp.likelihood.parameters(), 'lr':0.1}])
+    # optimizer = torch.optim.Adam([{'params': gp.im_net.parameters(), 'lr': 1e-3},
+    #                               {'params': gp.pol_net.parameters(), 'lr': 1e-3},
+    #                               {'params': gp.covar_module.parameters(), 'lr': 1e-3},
+    #                               {'params': gp.likelihood.parameters(), 'lr': 1e-3}])
+    optimizer = torch.optim.Adam([{'params': gp.parameters(), 'lr': 1e-3}])
+
     mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, gp)
     val_loss_fn = torch.nn.MSELoss()
     best = 1000
+
+    # pretrain_modules(gp, train_x, train_y)
+
     with gpytorch.settings.max_preconditioner_size(200), gpytorch.settings.max_cg_iterations(10000), gpytorch.settings.fast_computations(log_prob=False, solves=False, covar_root_decomposition=False):
-        for tx in range(6000):
+        for tx in range(50000):
             optimizer.zero_grad()
             output = gp(train_xs)
             loss = -mll(output, train_y)
@@ -271,8 +465,11 @@ if __name__ == '__main__':
                 #likelihood.train()
                 
                 #if loss.item() < best:
-            best = loss.item()
-            torch.save((gp.state_dict(), train_xs, train_y, mu, std), args.save_path)
+            
+            if tx % 50 == 0 and loss.item() < best:
+                print('Saving to', args.save_path)
+                torch.save((gp.state_dict(), train_xs, train_y, mu, std), args.save_path)
+                best = loss.item()
     gp.eval()
     print('Val Predictions')
     for ix in range(0, 20):#val_x.shape[0]):

learning/modules/image_encoder_spatialsoftmax.py
@@ -33,6 +33,21 @@ class ImageEncoder(nn.Module):
         self.sm = nn.Softmax(dim=1)
         self.temp = nn.Parameter(torch.tensor(1.0))
 
+    def get_features(self, img):
+        img = img[:, :, 1:, 1:]
+        bs, c, h, w = img.shape
+
+        img = self.pad(img)
+        x = F.relu(self.conv1(img))
+        x = self.pad(x)
+        x = self.conv2(x)
+
+        # Do a spatial softmax.
+        bs, c, h, w = x.shape
+        features = self.sm(x.view(bs*c, -1)/self.temp)
+
+        return features.view(bs, c*h*w) 
+
     def forward(self, img):
         img = img[:, :, 1:, 1:]
         bs, c, h, w = img.shape
@@ -53,7 +68,7 @@ class ImageEncoder(nn.Module):
         # Do a spatial softmax.
         bs, c, h, w = x.shape
         features = self.sm(x.view(bs*c, -1)/self.temp)
-
+        
         # Get expected feature points.
         pfeatures = features.view([-1, h, w])
 

utils/plot_uncertainty.py
@@ -7,7 +7,7 @@ from learning.modules.image_encoder_spatialsoftmax import ImageEncoder as Spatia
 from learning.modules.image_encoder import ImageEncoder as CNNEncoder
 from learning.models.nn_disp_pol_vis import DistanceRegressor
 from learning.dataloaders import setup_data_loaders, parse_pickle_file, PolicyDataset
-from learning.models.nn_with_kernel import FeatureExtractor, DistanceGP
+from learning.models.nn_with_kernel import FeatureExtractor, DistanceGP, ProductDistanceGP
 import gpytorch
 from gpytorch.kernels import GridInterpolationKernel, ScaleKernel, RBFKernel
 from utils.util import load_model, read_from_file
@@ -100,8 +100,8 @@ if __name__ == '__main__':
 
     # Load the GP and Feature Extractor.
     gp_state, train_xs, train_ys, mu, std = torch.load(args.gp_path)
-    likelihood = gpytorch.likelihoods.GaussianLikelihood(noise_constraint=gpytorch.constraints.Interval(1e-8, 6.25e-6))
-    gp = DistanceGP(train_x=train_xs,
+    likelihood = gpytorch.likelihoods.GaussianLikelihood(noise_constraint=gpytorch.constraints.Interval(1e-8, 1.e-6))
+    gp = ProductDistanceGP(train_x=train_xs,
                     train_y=train_ys,
                     likelihood=likelihood)
     extractor = FeatureExtractor(pretrained_nn_path=args.pretrained_nn)
@@ -117,7 +117,7 @@ if __name__ == '__main__':
     #raw_results = read_from_file('100_eval_doors.pickle')
     #val_results = [bb[::] for bb in raw_results]
     #val_results = [item for sublist in val_results for item in sublist]
-    val_results = [bb[::] for bb in raw_results[82:]]
+    val_results = [bb[::] for bb in raw_results[80:]]
     val_results = [item for sublist in val_results for item in sublist]
     val_data = parse_pickle_file(val_results)
     val_set  = setup_data_loaders(data=val_data, batch_size=16, single_set=True)
@@ -125,6 +125,7 @@ if __name__ == '__main__':
     val_x = (val_x - mu)/std
     
     gp.eval()
+    #gp.set_train_data(train_xs[::10,:], train_ys[::10], strict=False)
     likelihood.eval()
     print('Val Predictions')
     with gpytorch.settings.max_preconditioner_size(200), gpytorch.settings.max_cg_iterations(10000), gpytorch.settings.fast_computations(log_prob=False, solves=False, covar_root_decomposition=False):